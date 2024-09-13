dcf8b28b3287fa68ab63543ee1c1e9ce507e62a4
SoyElTeje <91272417+SoyElTeje@users.noreply.github.com>
2024-09-13 18:50:00 -0300

    main de prueba para hash

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include "tads/HashEjercicio1.cpp"
 
 int main() {
-    
+    TablaHash tabla(100);
+    tabla.insertar("asd", "Libro 1");
+    tabla.insertar("dsa", "Libro 2");
+    tabla.imprimir();
+    cout << tabla.recuperar("asd") << endl;
+    cout << tabla.recuperar("dsa") << endl;
+    cout << tabla.recuperar("qwe") << endl;
+    return 0;
 }
