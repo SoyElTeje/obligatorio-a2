04a29b77333c26c1bb2fa2a1f61fa3c4713d4eec
SoyElTeje <91272417+SoyElTeje@users.noreply.github.com>
2024-10-05 16:44:09 -0300

    implementacion rehash

--- tads/HashEjercicio2.cpp
+++ tads/HashEjercicio2.cpp
@@ -5,8 +5,9 @@
 #include <iostream>
 using namespace std;
 
+
 struct Libro {
-    string id;
+    int id;
     string titulo;
     bool disponible;
 };
@@ -17,23 +18,15 @@ private:
     int largo;
     Libro** tabla;
     bool* ocupado;
-    int habilidados;
+    int habilitados;
     int deshabilitados;
 
-    int hash1(string clave) {
-        int hash = 0;
-        for (int i = 0; i < clave.length(); i++) {
-            hash += clave[i];
-        }
-        return hash % largo;
+    int hash1(int clave) {
+        return clave % largo;
     }
 
-    int hash2(string clave) {
-        int hash = 0;
-        for (int i = 0; i < clave.length(); i++) {
-            hash += clave[i];
-        }
-        return (hash % (largo - 1)) + 1;
+    int hash2(int clave) {
+        return (clave + 1) % largo;
     }
 
     int siguientePrimo(int n) {
@@ -53,16 +46,54 @@ private:
         }
     }
 
-    void insertarAux(string id, string titulo) {
+    void rehash() {
+        int nuevoLargo = siguientePrimo(largo * 2);
+        Libro** nuevaTabla = new Libro*[nuevoLargo];
+        bool* nuevoOcupado = new bool[nuevoLargo];
+
+        for(int i = 0; i < nuevoLargo; i++) {
+            nuevaTabla[i] = NULL;
+            nuevoOcupado[i] = false;
+        }
+
+        for(int i = 0; i < nuevoLargo; i++) {
+            if(ocupado[i]) {
+                int nuevoPos = hash1(tabla[i]->id);
+                if(!nuevoOcupado[nuevoPos]) {
+                    nuevaTabla[nuevoPos] = tabla[i];
+                    nuevoOcupado[nuevoPos] = true;
+                }
+                else {
+                    int nuevoPos2 = hash2(tabla[i]->id);
+                    int intento = 0;
+                    while (nuevoOcupado[nuevoPos] && intento < nuevoLargo) {
+                        nuevoPos = (nuevoPos + intento * nuevoPos2) % nuevoLargo;
+                        intento++;
+                    }
+                    nuevaTabla[nuevoPos] = tabla[i];
+                    nuevoOcupado[i] = true;
+                }
+            }
+        }
+        delete[] tabla;
+        delete[] ocupado;
+
+        tabla = nuevaTabla;
+        ocupado = nuevoOcupado;
+        largo = nuevoLargo;
+    }
+    
+    void insertarAux(int id, string titulo) {
+        int factorCarga = (habilitados + deshabilitados) / largo;
+        if (factorCarga > 0.7) rehash();
         int pos = hash1(id);
-        cout << "Posicion: " << pos << endl;
         if (!ocupado[pos]) {
             tabla[pos] = new Libro();
             tabla[pos]->id = id;
             tabla[pos]->titulo = titulo;
             tabla[pos]->disponible = true;
             ocupado[pos] = true;
-            habilidados++;
+            habilitados++;
         }
         else {
             if(tabla[pos]->id == id) {
@@ -71,11 +102,9 @@ private:
                 return;
             }
             int pos2 = hash2(id);
-            cout << "Posicion 2: " << pos2 << endl;
             int intento = 0;
             while (ocupado[pos] && intento < largo) {
                 pos = (pos + intento * pos2) % largo;
-                cout << "Posicion aux: " << pos << endl;
                 intento++;
             }
             if (ocupado[pos]) {
@@ -91,14 +120,16 @@ private:
                 tabla[pos]->titulo = titulo;
                 ocupado[pos] = true;
             }
-            habilidados++;
+            habilitados++;
         }
     }
 
-    string recuperarAux(string id) {
+    void recuperarAux(int id) {
+        bool encontrado = false;
         int pos = hash1(id);
         if(ocupado[pos] && tabla[pos]->id == id) {
-            return tabla[pos]->titulo;
+            cout << tabla[pos]->titulo  << " " << (tabla[pos]->disponible ? "H" : "D") << endl;
+            return;
         }
         int pos2 = hash2(id);
         int intento = 0;
@@ -106,24 +137,26 @@ private:
             pos = (pos + intento * pos2) % largo;
             intento++;
             if(ocupado[pos] && tabla[pos]->id == id) {
-                return tabla[pos]->titulo;
+                cout << tabla[pos]->titulo  << " " << (tabla[pos]->disponible ? "H" : "D") << endl;
+                encontrado = true;
             }
         }
-        return "libro_no_encontrado";
+        if (encontrado) cout << "libro_no_encontrado" << endl;
     }
 
-    string cambioEstadoAux(string id) {
+    void cambioEstadoAux(int id) {
         int pos = hash1(id);
+        bool encontrado = false;
         if(ocupado[pos] && tabla[pos]->id == id) {
             tabla[pos]->disponible = !tabla[pos]->disponible;
             if(tabla[pos]->disponible) {
-                habilidados++;
+                habilitados++;
                 deshabilitados--;
             } else {
-                habilidados--;
+                habilitados--;
                 deshabilitados++;
             }
-            return tabla[pos]->disponible ? "cambio_a_libro_disponible" : "cambio_a_libro_no_disponible";
+            encontrado = true;
         }
         int pos2 = hash2(id);
         int intento = 0;
@@ -133,23 +166,23 @@ private:
             if(ocupado[pos] && tabla[pos]->id == id) {
                 tabla[pos]->disponible = !tabla[pos]->disponible;
                 if(tabla[pos]->disponible) {
-                    habilidados++;
+                    habilitados++;
                     deshabilitados--;
                 } else {
-                    habilidados--;
+                    habilitados--;
                     deshabilitados++;
                 }
-                return tabla[pos]->disponible ? "cambio_a_libro_disponible" : "cambio_a_libro_no_disponible";
+                encontrado = true;
             }
         }
-        return "libro_no_encontrado";
+        if (!encontrado) cout << "libro_no_encontrado" << endl;
     }
 public:
     TablaHash(int tamano) {
         largo = this->siguientePrimo(tamano);
         tabla = new Libro*[largo];
         ocupado = new bool[largo];
-        habilidados = 0;
+        habilitados = 0;
         deshabilitados = 0;
         for (int i = 0; i < largo; i++) {
             tabla[i] = nullptr;
@@ -167,28 +200,26 @@ public:
         delete[] ocupado;
     }
 
-    void insertar(string id, string titulo) {
-        cout << "Insertando " << id << " - " << titulo << endl;
+    void insertar(int id, string titulo) {
         insertarAux(id, titulo);
     }
 
     void imprimir() {
         for (int i = 0; i < largo; i++) {
             if (ocupado[i]) {
-                cout << "Posicion " << i << ": " << tabla[i]->id << " - " << tabla[i]->titulo << endl;
             }
         }
     }
 
-    string recuperar(string id) {
-        return recuperarAux(id);
+    void recuperar(int id) {
+        recuperarAux(id);
     }
 
     string informe() {
-        return "Libros habilitados: " + to_string(habilidados) + ", libros deshabilitados: " + to_string(deshabilitados);
+        cout << to_string(habilitados + deshabilitados) << " " << to_string(habilitados) << " " << to_string(deshabilitados) << endl;
     }
 
-    string cambioEstado(string id) {
+    void cambioEstado(int id) {
         return cambioEstadoAux(id);
     }
 };
