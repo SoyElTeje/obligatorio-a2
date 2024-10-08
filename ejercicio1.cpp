4f583848d9cddb54d3ef255ca9919d79f25c4d73
SoyElTeje <91272417+SoyElTeje@users.noreply.github.com>
2024-10-08 12:56:45 -0300

    feat: ejercicio 1

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,5 +7,28 @@ using namespace std;
 
 int main()
 {
+    int cantidadElementos;
+    cin >> cantidadElementos;
+    TablaHash tabla(cantidadElementos);
+    string entrada = "";
+    while(cin >> entrada) {
+        if(entrada == "ADD") {
+            int id;
+            string titulo;
+            cin >> id;
+            cin >> titulo;
+            tabla.insertar(id, titulo);
+        }
+        else if (entrada == "FIND") {
+            int id;
+            cin >> id;
+            cout << tabla.recuperar(id) << endl;
+        }
+        else if (entrada == "TOGGLE") {
+            int id;
+            cin >> id;
+            tabla.cambioEstado(id);
+        }
+    }
     return 0;
 }
