e425f484e4c74e36eb452c1796fa61409d53ab5a
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2024-11-12 14:50:05 -0300

    heapEjercicio3 correcto

--- tads/Heap/heapEjercicio3.cpp
+++ tads/Heap/heapEjercicio3.cpp
@@ -13,58 +13,77 @@ class Heap {
     nodoHeap* vec;
     int ultimaPos;
 
+    void swap(nodoHeap &a, nodoHeap &b) {
+        nodoHeap aux = a;
+        a = b;
+        b = aux;
+    }
 
     void agregarElementoAux(int id, int precio) {
+        int posRepetido = estaEnHeap(id);
         vec[ultimaPos].id = id;
         vec[ultimaPos].precio = precio;
 
-        int i = ultimaPos;
-        while (i > 1 && precio < vec[i/2].precio) {
-            nodoHeap aux = vec[i];
-            vec[i] = vec[i/2];
-            vec[i/2] = aux;
+        if (posRepetido != 0) {
+            if (precio < vec[posRepetido].precio) {
+                vec[posRepetido].precio = precio;
+                flotar(posRepetido);
+            } else if (precio == vec[posRepetido].precio && id > vec[posRepetido].id) {
+                vec[posRepetido].id = id;
+                flotar(posRepetido);
+            }
+        } else {
+            vec[ultimaPos].id = id;
+            vec[ultimaPos].precio = precio;
+            flotar(ultimaPos);
+            ultimaPos++;
+        }
+    }
+
+    void flotar(int i) {
+        while (i > 1 && (vec[i].precio < vec[i/2].precio || 
+            (vec[i].precio == vec[i/2].precio && vec[i].id > vec[i/2].id))) { // precios iguales e ID nuevo mayor que id viejo
+            swap(vec[i], vec[i/2]);
             i = i / 2;
         }
-        ultimaPos++;
     }
 
-    bool noEstaEnHeap(int id) {
-        for (int i = 0; i < largo; i++) {
-            if (vec[i].id == id) return false;
-        }
-        return true;
-    }
-
-    void pop() {
-        cout << vec[1].id << endl;
-        if (ultimaPos < 1) {
-            return;
-        }
-
-        vec[1] = vec[ultimaPos];
-        ultimaPos--;
-        
-        int i = 1;
-
-        while (true) {
+    void hundir(int i) {
+        while (i * 2 < ultimaPos) {
             int nodoIzq = i * 2;
             int nodoDer = (i * 2) + 1;
             int masChico = i;
-            
-            if (nodoIzq <= ultimaPos && vec[nodoIzq].precio < vec[nodoDer].precio) {
+
+            if (nodoIzq < ultimaPos && (vec[nodoIzq].precio < vec[masChico].precio || (vec[nodoIzq].precio == vec[masChico].precio && vec[nodoIzq].id > vec[masChico].id))) {
                 masChico = nodoIzq;
             }
-            if (nodoDer <= ultimaPos && vec[nodoIzq].precio > vec[nodoDer].precio) {
+            if (nodoDer < ultimaPos && (vec[nodoDer].precio < vec[masChico].precio ||
+                (vec[nodoDer].precio == vec[masChico].precio && vec[nodoDer].id > vec[masChico].id))) {
                 masChico = nodoDer;
             }
             if (masChico != i) {
-                nodoHeap aux = vec[i];
-                vec[i] = vec[masChico];
-                vec[masChico] = aux;
+                swap(vec[i], vec[masChico]);
                 i = masChico;
             } else {
                 break;
             }
+        }
+    }
+
+    int estaEnHeap(int id) {
+        for (int i = 1; i < ultimaPos; i++) {
+            if (vec[i].id == id) return i;
+        }
+        return 0;
+    }
+
+    void pop(int k) {
+        while (k > 0 && ultimaPos > 1) {
+            cout << vec[1].id << endl;
+            vec[1] = vec[ultimaPos - 1];
+            ultimaPos--;
+            hundir(1);
+            k--;
         }
     }
 
@@ -76,15 +95,10 @@ class Heap {
     }
 
     void agregarElemento(int id, int precio){
-        if (noEstaEnHeap(id)) {
-            agregarElementoAux(id, precio);
-        }
-        
+        agregarElementoAux(id, precio);        
     };
 
     void mostrtarKelementos(int k) {
-        for (int i = 1; i <= k && i <= ultimaPos; i++) {
-            pop();
-        }
+        pop(k);
     };
 };
