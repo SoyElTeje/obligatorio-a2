7d087b778176cbb4891fc167d793413d03fde414
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2024-11-12 09:10:32 -0300

    main ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,11 +2,26 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/Heap/heapEjercicio3.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
+    int cantidadElementos;
+    cin >> cantidadElementos;
+    Heap heap(cantidadElementos);
+
+    for (int i = 0; i < cantidadElementos; i++) {
+        int id;
+        int precio;
+        cin >> id;
+        cin >> precio;
+        heap.agregarElemento(id, precio);
+    }
+    int k;
+    cin >> k;
+    heap.mostrtarKelementos(k);
+
     return 0;
 }
