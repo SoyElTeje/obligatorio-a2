ef0ab31d200cd3700a7419939cca42fd23fa3be7
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2024-11-27 10:38:55 -0300

    ejercicio 7, casos de prueba aprobados

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,11 +2,26 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/Greedy/GreedyEj7.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
+
+    int cantidadCanciones;
+    cin >> cantidadCanciones;
+
+    int* canciones = new int [cantidadCanciones];
+    for (int i = 0; i < cantidadCanciones; i++) {
+        cin >> canciones[i];
+    }
+
+    int cantidadEdtudiantes;
+    cin >> cantidadEdtudiantes;
+
+    GreedyCanciones* procesarCanciones = new GreedyCanciones(cantidadCanciones, cantidadEdtudiantes, canciones);
+    procesarCanciones->calcularResultado();
+
     return 0;
 }
