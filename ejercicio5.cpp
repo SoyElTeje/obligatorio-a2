433610b8e90c6f018698f2aadba856d99ff91156
SoyElTeje <91272417+SoyElTeje@users.noreply.github.com>
2024-11-28 16:23:03 -0300

    fixes

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -63,3 +63,113 @@ int main()
     misiones->setNombresMisiones(nombresMisiones);
     misiones->imprimirOrdenIdeal(origen);
 }
+
+// #include <cassert>
+// #include <string>
+// #include <iostream>
+// #include <climits>
+// #include "./tads/OT/laGodClase.cpp"
+
+// using namespace std;
+
+// int main()
+// {
+//     // Ingreso de Misiones
+//     int cantidadMisiones;
+//     cin >> cantidadMisiones;
+//     GrafoMision *grafoMisiones = new GrafoMision(cantidadMisiones);
+
+//     int idMision;
+//     string nombreMision;
+//     int idCiudad;
+//     int dependientes;
+
+//     for (int i = 1; i <= cantidadMisiones; i++)
+//     {
+//         cin >> idMision;
+//         cin >> nombreMision;
+//         cin >> idCiudad;
+//         grafoMisiones->agregarMision(idMision, nombreMision, idCiudad);
+
+//         cin >> dependientes;
+//         while (dependientes != 0)
+//         {
+//             grafoMisiones->agregarDependientes(idMision, dependientes);
+//             cin >> dependientes;
+//         }
+//     }
+
+//     // Ingreso de Ciudades
+//     int cantidadCiudades;
+//     cin >> cantidadCiudades;
+//     int posInicialIMF;
+//     cin >> posInicialIMF;
+
+//     GrafoCiudad *grafoCiudades = new GrafoCiudad(cantidadCiudades);
+//     int idCiudadparaCiudades;
+//     string nombreCiudad;
+//     for (int i = 1; i <= cantidadCiudades; i++)
+//     {
+//         cin >> idCiudad;
+//         cin >> nombreCiudad;
+//         grafoCiudades->insertarCiudad(nombreCiudad, idCiudad);
+//     }
+
+//     // Conecciones entre ciudades
+//     int cantidadConecciones;
+//     cin >> cantidadConecciones;
+//     int idCiudadOrigen;
+//     int idCiudadDestino;
+//     int tiempoDesplazamiento;
+
+//     for (int i = 1; i <= cantidadConecciones; i++)
+//     {
+//         cin >> idCiudadOrigen;
+//         cin >> idCiudadDestino;
+//         cin >> tiempoDesplazamiento;
+//         grafoCiudades->agregarConexionCiudad(idCiudadOrigen, idCiudadDestino, tiempoDesplazamiento);
+//     }
+
+//     // Procesar los datos
+//     OT *ot = new OT();
+//     Lista *ordenTopologico = ot->orden(grafoMisiones, grafoCiudades, posInicialIMF);
+
+//     int tiempoTotal = 0;
+//     int posicionIMF = posInicialIMF;
+
+//     cout << "Ciudad inicial: " << grafoCiudades->obtenerNombreCiudad(posicionIMF) << endl;
+//     nodoLista *actual = ordenTopologico->getCabeza();
+
+//     while (actual != NULL)
+//     {
+//         int idMision = actual->idMision;
+//         int idCiudadMision = grafoMisiones->getCiudad(idMision);
+
+//         // Obtener detalles de la misión
+//         string nombreMision = grafoMisiones->getNombreMision(idMision);
+//         string nombreCiudadMision = grafoCiudades->obtenerNombreCiudad(idCiudadMision);
+//         int *costos = grafoCiudades->dijkstra(posicionIMF);
+//         int tiempoViaje = costos[idCiudadMision];
+//         // Imprimir detalles de la misión
+//         cout << grafoCiudades->obtenerNombreCiudad(posicionIMF) << " -> "
+//              << nombreCiudadMision << " -> Mision: " << nombreMision
+//              << " - " << nombreCiudadMision
+//              << " - Tiempo de viaje: " << tiempoViaje << endl;
+//         // Actualizar tiempo total y posición actual
+//         tiempoTotal += tiempoViaje;
+//         posicionIMF = idCiudadMision;
+//         // Liberar memoria de los costos de Dijkstra
+//         delete[] costos;
+
+//         actual = actual->sig;
+//     }
+
+//     cout << "Misiones ejecutadas con éxito." << endl;
+//     cout << "Tiempo total de viaje: " << tiempoTotal << endl;
+
+//     delete grafoMisiones;
+//     delete grafoCiudades;
+//     delete ot;
+
+//     return 0;
+// }
--- pruebas.sh
+++ pruebas.sh
@@ -7,91 +7,97 @@ Green='\033[0;32m'
 
 java=false
 for i in *.java; do
-	if [ -f "$i" ]; then
-		java=true
-	fi
+    if [ -f "$i" ]; then
+        java=true
+    fi
 done
 
 existe() {
-	if [ ! -f "$1" ]; then
-		return 2
-	fi
+    if [ ! -f "$1" ]; then
+        return 2
+    fi
 }
 
 compilar() {
-	if "$java"; then
-		javac Ejercicio"$1".java || {
-			echo "Error de compilación. Abortando prueba"
-			return 1
-		}
-	else
-		g++ -std=c++11 ejercicio"$1".cpp -o ejercicio"$1".out || {
-			echo "Error de compilación. Abortando prueba"
-			return 1
-		}
-	fi
+    if "$java"; then
+        javac Ejercicio"$1".java || {
+            echo "Error de compilación. Abortando prueba"
+            return 1
+        }
+    else
+        g++ -std=c++11 ejercicio"$1".cpp -o ejercicio"$1".out || {
+            echo "Error de compilación. Abortando prueba"
+            return 1
+        }
+    fi
 }
 
 comparar() {
-	echo '---------------------------'
-	echo -n "$2.in.txt: "
-	if "$java"; then
-		java Ejercicio"$1" <tests/Ejercicio"$1"/"$2".in.txt >tests/Ejercicio"$1"/"$2".out.mine.txt || {
-			echo "Error de ejecución"
-			return 1
-		}
-	else
-		./ejercicio"$1".out <tests/Ejercicio"$1"/"$2".in.txt >tests/Ejercicio"$1"/"$2".out.mine.txt || {
-			echo "Error de ejecución"
-			return 1
-		}
-	fi
-	output=$(diff --strip-trailing-cr tests/Ejercicio"$1"/"$2".out.mine.txt tests/Ejercicio"$1"/"$2".out.txt)
-	if [ -z "$output" ]; then
-		echo -e "${Green}OK${Color_Off}"
-		rm tests/Ejercicio"$1"/"$2".out.mine.txt 2>/dev/null
-	else
-		echo -e "${Red}ERROR${Color_Off}"
-	fi
+    echo '---------------------------'
+    echo -n "$2.in.txt: "
+    start=$(date +%s.%N)  # Captura el tiempo inicial en segundos con precisión decimal
+    if "$java"; then
+        java Ejercicio"$1" <tests/Ejercicio"$1"/"$2".in.txt >tests/Ejercicio"$1"/"$2".out.mine.txt || {
+            echo "Error de ejecución"
+            return 1
+        }
+    else
+        ./ejercicio"$1".out <tests/Ejercicio"$1"/"$2".in.txt >tests/Ejercicio"$1"/"$2".out.mine.txt || {
+            echo "Error de ejecución"
+            return 1
+        }
+    fi
+    end=$(date +%s.%N)  # Captura el tiempo final en segundos con precisión decimal
+
+    # Calcula la duración en segundos
+    duration=$(echo "$end - $start" | bc)
+
+    output=$(diff --strip-trailing-cr tests/Ejercicio"$1"/"$2".out.mine.txt tests/Ejercicio"$1"/"$2".out.txt)
+    if [ -z "$output" ]; then
+        echo -e "${Green}OK${Color_Off} (${duration}s)"
+        rm tests/Ejercicio"$1"/"$2".out.mine.txt 2>/dev/null
+    else
+        echo -e "${Red}ERROR${Color_Off} (${duration}s)"
+    fi
 }
 
 correrPrueba() {
-	if "$java"; then
-		existe Ejercicio"$1".java || return 2
-	else
-		existe ejercicio"$1".cpp || return 2
-	fi
-	echo '---------------------------'
-	echo "        Ejercicio $1       "
-	compilar "$1" || return 1
-	for i in $(ls -v tests/Ejercicio"$1"/*.in.txt); do
-		nombre=$(basename "$i" .in.txt)
-		comparar "$1" "$nombre"
-	done
+    if "$java"; then
+        existe Ejercicio"$1".java || return 2
+    else
+        existe ejercicio"$1".cpp || return 2
+    fi
+    echo '---------------------------'
+    echo "        Ejercicio $1       "
+    compilar "$1" || return 1
+    for i in $(ls -v tests/Ejercicio"$1"/*.in.txt); do
+        nombre=$(basename "$i" .in.txt)
+        comparar "$1" "$nombre"
+    done
 }
 
 if [[ $# -eq 0 ]]; then
-	for i in $(eval echo "{1..$cantEjercicios}"); do
-		correrPrueba "$i"
-	done
+    for i in $(eval echo "{1..$cantEjercicios}"); do
+        correrPrueba "$i"
+    done
 fi
 
 while [[ $# -gt 0 ]]; do
-	if "$java"; then
-		ext="java"
-		nombre="Ejercicio"
-	else
-		nombre="ejercicio"
-		ext="cpp"
-	fi
-	if [[ $1 -lt 1 || $1 -gt $cantEjercicios ]]; then
-		echo "Ejercicio $1: inválido / no soportado"
-		shift
-		continue
-	fi
-	correrPrueba "$1"
-	if [[ $? -eq 2 ]]; then echo "No existe $nombre$1.$ext"; fi
-	shift
+    if "$java"; then
+        ext="java"
+        nombre="Ejercicio"
+    else
+        nombre="ejercicio"
+        ext="cpp"
+    fi
+    if [[ $1 -lt 1 || $1 -gt $cantEjercicios ]]; then
+        echo "Ejercicio $1: inválido / no soportado"
+        shift
+        continue
+    fi
+    correrPrueba "$1"
+    if [[ $? -eq 2 ]]; then echo "No existe $nombre$1.$ext"; fi
+    shift
 done
 
 rm ./*.out 2>/dev/null
